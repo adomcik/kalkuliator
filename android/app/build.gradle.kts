ee24b6728b5ac62fc06614e96650c6fa045b4901
Adomas <ado.glodas@gmail.com>
2025-05-12 00:56:02 +0300

    .

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.kalkuliator"
+    namespace = "com.example.kalkuliator"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
